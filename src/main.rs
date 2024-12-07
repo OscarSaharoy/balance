355caa011433ba33d333ab2aeaa042e5a1a56864
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2024-12-07 00:04:46 +0000

    input

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
 use leptos::prelude::*;
 
 #[component]
-fn Space(height: &str) -> impl IntoView {
+fn Space<'a>(height: &'a str) -> impl IntoView + use<'a> {
     view! {
-        <span style:height=height />
+        <div style:margin-top=height />
     }
+}
+
+fn get_response(foods: String) -> String {
+    "test".to_string() + &foods
 }
 
 #[component]
 fn Intro() -> impl IntoView {
     view! {
-        <h1> balance </h1>
-        <space height="1rem" />
+        <h1> "balance ⚖️ " </h1>
         <p> What have you eaten today? </p>
     }
 }
 
 #[component]
+fn Foods() -> impl IntoView {
+    let (foods, set_foods) = signal("".to_string());
+
+    view! {
+        <input
+            on:input:target=move |e| set_foods.set(e.target().value()) 
+            value=foods
+            placeholder="eg. Bread, Brazil Nuts, Milk"
+            style="font-size: 1rem;"
+        />
+        <p> {move || get_response(foods.get())} </p>
+    }
+}
+
+
+#[component]
 fn App() -> impl IntoView {
     view! {
         <div style="min-width: 100vw; min-height: 100vh; padding: 2rem; display: grid; place-items: center;">
-            <main style="width: 100%; max-width: 50rem;">
+            <main style="width: 100%; max-width: 50rem; display: grid; gap: 1rem;">
                 <Intro />
+                <Foods />
             </main>
         </div>
     }
