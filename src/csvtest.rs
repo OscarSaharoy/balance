7e624b44201edae076112c63b05c37a52d375501
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2024-12-09 17:04:56 +0000

    csv iterating

--- src/csvtest.rs
+++ src/csvtest.rs
@@ -7,6 +7,15 @@ enum NutrientValue {
 }
 
 #[derive(Debug)]
+struct Nutrient {
+    name: String,
+    display_name: String,
+    abbreviation: String,
+    units: String,
+    recommended_intake: f64,
+}
+
+#[derive(Debug)]
 struct Food {
     name: String,
     nutrients: HashMap<String, NutrientValue>,
@@ -20,7 +29,14 @@ fn main() -> () {
         Err(g) => panic!("{}", g),
     };
     let nutrients = Vec::<Nutrient>::new();
-    let mut rdr = csv::Reader::from_reader(message.as_bytes());
+    let mut rdr = csv::ReaderBuilder::new().has_headers(false).from_reader(message.as_bytes());
+    let mut headers = rdr.records().take(5);
+    while let Some(Ok(result)) = headers.next() {
+        for r in &result {
+            println!("{r}");
+        }
+    }
+
     for result in rdr.records() {
         match result {
             Ok(record) => println!("{record:?}"),
