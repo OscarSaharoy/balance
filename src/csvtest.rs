f3c8cffe892db517cf80e6c554af42e8d2d84ea5
Oscar Saharoy <osaharoy@gmail.com>
2024-12-10 09:30:28 +0000

    parsing headers

--- src/csvtest.rs
+++ src/csvtest.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use csv::StringRecord;
 
 #[derive(Debug)]
 enum NutrientValue {
@@ -12,7 +13,7 @@ struct Nutrient {
     display_name: String,
     abbreviation: String,
     units: String,
-    recommended_intake: f64,
+    recommended_intake: f32,
 }
 
 #[derive(Debug)]
@@ -22,20 +23,41 @@ struct Food {
 }
 
 fn main() -> () {
-    let message: String = match std::fs::read_to_string(
+    let message: String = std::fs::read_to_string(
         "./assets/cofid.csv"
-    ) {
-        Ok(s) => s,
-        Err(g) => panic!("{}", g),
-    };
-    let nutrients = Vec::<Nutrient>::new();
-    let mut rdr = csv::ReaderBuilder::new().has_headers(false).from_reader(message.as_bytes());
-    let mut headers = rdr.records().take(5);
-    while let Some(Ok(result)) = headers.next() {
-        for r in &result {
-            println!("{r}");
-        }
+    ).expect("cofid.csv is error free");
+
+    let mut nutrients = Vec::<Nutrient>::new();
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(message.as_bytes());
+    let mut headers: Vec<Vec<String>> = rdr
+        .records()
+        .take(5)
+        .map(|r| r
+            .expect("cofid.csv is error free")
+            .into_iter()
+            .map(|s| s.to_owned())
+            .collect()
+        )
+        .collect();
+
+    for i in 0..headers[0].len() {
+        let recommended_intake: f32 = headers[4]
+            .remove(0)
+            .parse()
+            .map_or(0., |s| s);
+        let n = Nutrient {
+            name: headers[0].remove(0),
+            display_name: headers[3].remove(0),
+            abbreviation: headers[2].remove(0),
+            units: headers[1].remove(0),
+            recommended_intake: recommended_intake,
+        };
+        nutrients.push(n);
     }
+
+    return;
 
     for result in rdr.records() {
         match result {
