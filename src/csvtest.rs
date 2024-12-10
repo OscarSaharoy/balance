b2712a9abcdf690a8b0e410fab6f96c3578209cc
Oscar Saharoy <osaharoy@gmail.com>
2024-12-10 21:14:46 +0000

    csv parsed

--- src/csvtest.rs
+++ src/csvtest.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use csv::StringRecord;
 
 #[derive(Debug)]
 enum NutrientValue {
@@ -16,22 +15,38 @@ struct Nutrient {
     recommended_intake: f32,
 }
 
+
 #[derive(Debug)]
 struct Food {
     name: String,
     nutrients: HashMap<String, NutrientValue>,
 }
 
-fn main() -> () {
-    let message: String = std::fs::read_to_string(
-        "./assets/cofid.csv"
-    ).expect("cofid.csv is error free");
+fn make_food(
+    record: csv::StringRecord,
+    mut nutrient_names: Vec<String>,
+) -> Food {
+    let name = record
+        .get(0)
+        .expect("each row has at least 1 record")
+        .to_owned();
+    let values = record
+        .iter()
+        .map(|x| match x.parse::<f32>() {
+            Ok(f) => NutrientValue::Value(f),
+            Err(_) => NutrientValue::Code(x.to_owned()),
+        });
+    Food {
+        name: name,
+        nutrients: values
+            .map(|x| (nutrient_names.remove(0), x))
+            .collect::<HashMap<String, NutrientValue>>(),
+    }
+}
 
+fn take_headers(reader: &mut csv::Reader<&[u8]>) -> Vec<Nutrient> {
     let mut nutrients = Vec::<Nutrient>::new();
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(false)
-        .from_reader(message.as_bytes());
-    let mut headers: Vec<Vec<String>> = rdr
+    let mut headers: Vec<Vec<String>> = reader
         .records()
         .take(5)
         .map(|r| r
@@ -41,28 +56,48 @@ fn main() -> () {
             .collect()
         )
         .collect();
-
-    for i in 0..headers[0].len() {
+    for _ in 0..headers[0].len() {
         let recommended_intake: f32 = headers[4]
             .remove(0)
             .parse()
             .map_or(0., |s| s);
-        let n = Nutrient {
+        let new_nutrient = Nutrient {
             name: headers[0].remove(0),
             display_name: headers[3].remove(0),
             abbreviation: headers[2].remove(0),
             units: headers[1].remove(0),
             recommended_intake: recommended_intake,
         };
-        nutrients.push(n);
+        nutrients.push(new_nutrient);
     }
+    return nutrients;
+}
 
-    return;
+fn get_foods(csv: String) -> Vec<Food> {
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(csv.as_bytes());
 
-    for result in rdr.records() {
-        match result {
-            Ok(record) => println!("{record:?}"),
-            Err(g) => println!("{}", g),
-        }
-    }
+    let nutrients = take_headers(&mut reader);
+    let nutrient_names = nutrients
+        .iter()
+        .map(|n| n.name.clone())
+        .collect::<Vec<String>>();
+
+    reader
+        .records()
+        .map(|r| make_food(
+            r.expect("cofid.csv is error free"),
+            nutrient_names.clone(),
+        ))
+        .collect::<Vec<Food>>()
+}
+
+fn main() -> () {
+    let message: String = std::fs::read_to_string(
+        "./assets/cofid.csv"
+    ).expect("cofid.csv is error free");
+
+    let foods = get_foods(message);
+    println!("{foods:?}");
 }
